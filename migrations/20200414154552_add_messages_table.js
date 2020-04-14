1a43b4c379849f7617f9a97f8adb582d11a64b2d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-14 18:56:49 -0400

    Add username to schema

--- migrations/20200414154552_add_messages_table.js
+++ migrations/20200414154552_add_messages_table.js
@@ -1,6 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('messages', tbl => {
         tbl.increments();
+        tbl.string('username', 10).notNullable().defaultTo("Anon");
         tbl.string('message', 140).notNullable();
         tbl.timestamp('created_at').defaultTo(knex.fn.now());
     })
