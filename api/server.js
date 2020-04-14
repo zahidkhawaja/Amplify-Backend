6383ada8bc0643b7a86206b6ff40c7375244984a
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-14 16:01:25 -0400

    Add CORS and .gitignore

--- api/server.js
+++ api/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
+var cors = require("cors");
 const messagesRouter = require("../messages/messages-router");
 
 const server = express();
-
+server.use(cors());
 server.use(express.json());
-
 server.use("/api/messages", messagesRouter);
 
 server.get("/", (req, res) => {
