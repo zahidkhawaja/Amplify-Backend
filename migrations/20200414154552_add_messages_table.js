250f1844e863f8af745794221683a431556fc15d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-14 15:47:38 -0400

    Design schema, Knex migration

---
+++ migrations/20200414154552_add_messages_table.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('messages', tbl => {
+        tbl.increments();
+        tbl.string('message', 140).notNullable();
+        tbl.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('messages');
+};
