f5a2ae576cb8aecf672a67ea3e3aadd6c6f16820
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 18:48:18 -0400

    Setup authentication and verify token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,9 +7,7 @@ const Users = require("../users/users-model.js");
 router.post("/register", (req, res) => {
   let user = req.body;
   const rounds = process.env.HASH_ROUNDS || 8;
-
   const hash = bcrypt.hashSync(user.password, rounds);
-
   user.password = hash;
 
   Users.add(user)
@@ -24,18 +22,17 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
-  // Search for the user using the username
+  // Search for the user
   Users.findBy({ username })
     .then(user => {
-      // console.log("user", user[0]);
-      // If we find the user, then also check that the passwords match
+      // If we find the user and the passwords match
       if(user && bcrypt.compareSync(password, user[0].password)) {
         // Produce a token
         const token = generateToken(user);
         // Send the token to the client
-        res.status(200).json({ message: "Welcome!", token });
+        res.status(200).json({ message: "Login successful", token });
       } else {
-        res.status(401).json({ message: "You cannot pass!"})
+        res.status(401).json({ message: "Unable to login"})
       }
 
     })
@@ -51,7 +48,7 @@ function generateToken(user) {
     username:  user[0].username
   };
 
-  const secret = process.env.JWT_SECRET || "blahblahblah";
+  const secret = process.env.JWT_SECRET || "blah";
 
   const options = {
     expiresIn: "1d"
