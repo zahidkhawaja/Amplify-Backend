9688816a9d129bbcdac611aabd63842db67e79f6
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 18:20:18 -0400

    Add helmet middleware for security

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const helmet = require("helmet");
 var cors = require("cors");
+
 const messagesRouter = require("../messages/messages-router");
 
 const server = express();
+
+server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
 server.use("/api/messages", messagesRouter);
 
 server.get("/", (req, res) => {
