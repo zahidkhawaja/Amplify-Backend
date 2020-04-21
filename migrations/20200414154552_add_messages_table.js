8c3cde640457a8b90167bf5c618c5b3c6a344e96
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-04-21 18:27:53 -0400

    Knex migration, add users table

--- migrations/20200414154552_add_messages_table.js
+++ migrations/20200414154552_add_messages_table.js
@@ -1,12 +1,24 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('messages', tbl => {
+    return knex.schema
+    
+    // Messages table
+    .createTable("messages", tbl => {
         tbl.increments();
         tbl.string('username', 10).notNullable().defaultTo("Guest");
         tbl.string('message', 140).notNullable();
         tbl.timestamp('created_at').defaultTo(knex.fn.now());
     })
+
+    // Users table
+    .createTable("users", tbl => {
+        tbl.increments();
+        tbl.string("username", 128).notNullable().unique();
+        tbl.string("password", 128).notNullable();
+    })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('messages');
+    return knex.schema
+    .dropTableIfExists("users")
+    .dropTableIfExists("messages");
 };
